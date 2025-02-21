82c1c69c47d212d3fc4d90ac1ce2358a9e9e365d
BuildTools <unconfigured@null.spigotmc.org>
2025-02-21 17:00:32 +0200

    DeleteBoardModal

---
+++ src/components/DeleteBoardModal/DeleteBoardModal.jsx
@@ -0,0 +1,44 @@
+import "./DeleteBoardModal.scss";
+import Button from "../Button/Button";
+
+export function DeleteBoardModal({ isOpen, onClose, onConfirm, boardName }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="DeleteBoardModal">
+      <div
+        className="DeleteBoardModal-content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h3 className="DeleteBoardModal-title">Delete this board?</h3>
+        <p>
+          Are you sure you want to delete the '<strong>{boardName}</strong>'
+          board? This <br /> action will remove all columns and tasks and cannot
+          be reversed.
+        </p>
+        <div className="EditBoardModal-actions">
+          <Button
+            color="destructive"
+            size="sm"
+            width="px_200"
+            shadow="null"
+            opacity="0"
+            onClick={onConfirm}
+          >
+            Delete
+          </Button>
+          <Button
+            color="secondary"
+            size="sm"
+            width="px_200"
+            shadow="null"
+            opacity="null"
+            onClick={onClose}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import Button from "../Button/Button";
-// import "./Header.css";
 import "./Header.scss";
 import { useLocation, useParams } from "wouter";
 import { Api } from "../../api";
@@ -7,12 +6,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { boardsSlice } from "../../store";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { PencilIcon, TrashIcon } from "@heroicons/react/16/solid";
+import { DeleteBoardModal } from "../DeleteBoardModal/DeleteBoardModal";
+import { useState } from "react";
 
 export function Header({ openEditBoardModal, openAddNewTask }) {
   const [location, setLocation] = useLocation();
   const params = useParams();
   const dispatch = useDispatch();
   const selectedBoardName = params?.boardName;
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   const selectedBoard = useSelector((state) =>
     boardsSlice.selectors.selectedBoard(state, selectedBoardName)
@@ -24,6 +26,7 @@ export function Header({ openEditBoardModal, openAddNewTask }) {
     console.log("deleteBoard", deleteBoard);
     dispatch(boardsSlice.actions.deleteBoard(selectedBoard.id)); //update client
     setLocation("/"); // update
+    setIsDeleteModalOpen(false);
   }
 
   const isAddTaskDisabled =
@@ -31,67 +34,78 @@ export function Header({ openEditBoardModal, openAddNewTask }) {
     !selectedBoard.columns ||
     selectedBoard.columns.length === 0;
   return (
-    <div className="Header">
-      <p className="title"> Platform Launch</p>
-      <div className="add">
-        <Button
-          disabled={isAddTaskDisabled}
-          color="primary"
-          size="lg"
-          shadow="null"
-          width="scope_2"
-          opacity="0"
-          onClick={openAddNewTask}
-        >
-          + Add New Task
-        </Button>
-      </div>
-      <div className="actions">
-        <Menu>
-          <MenuButton className="dots">
-            <svg
-              className="menu-icon"
-              width="5"
-              height="20"
-              viewBox="0 0 5 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="2.30769" cy="2.30769" r="2.30769" fill="#828FA3" />
-              <circle cx="2.30769" cy="10.0001" r="2.30769" fill="#828FA3" />
-              <circle cx="2.30769" cy="17.6922" r="2.30769" fill="#828FA3" />
-            </svg>
-          </MenuButton>
-          <MenuItems transition anchor="bottom end" className="menuItems">
-            <MenuItem className="menuItem">
-              <span
-                onClick={() => {
-                  console.log("edit board");
-                  openEditBoardModal(selectedBoard);
-                }}
-                className=""
-                style={{ color: "#828fa3" }}
+    <>
+      <div className="Header">
+        <p className="title"> Platform Launch</p>
+        <div className="add">
+          <Button
+            disabled={isAddTaskDisabled}
+            color="primary"
+            size="lg"
+            shadow="null"
+            width="scope_2"
+            opacity="0"
+            onClick={openAddNewTask}
+          >
+            + Add New Task
+          </Button>
+        </div>
+        <div className="actions">
+          <Menu>
+            <MenuButton className="dots">
+              <svg
+                className="menu-icon"
+                width="5"
+                height="20"
+                viewBox="0 0 5 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <PencilIcon style={{ width: "24px", height: "24px" }} />
-                Edit Board
-              </span>
-            </MenuItem>
-            <MenuItem className="menuItem">
-              <span
-                onClick={() => {
-                  console.log("delete board");
-                  deleteBoard();
-                }}
-                className=""
-                style={{ color: "red" }}
-              >
-                <TrashIcon style={{ width: "24px", height: "24px" }} />
-                Delete Board
-              </span>
-            </MenuItem>
-          </MenuItems>
-        </Menu>
+                <circle cx="2.30769" cy="2.30769" r="2.30769" fill="#828FA3" />
+                <circle cx="2.30769" cy="10.0001" r="2.30769" fill="#828FA3" />
+                <circle cx="2.30769" cy="17.6922" r="2.30769" fill="#828FA3" />
+              </svg>
+            </MenuButton>
+            <MenuItems transition anchor="bottom end" className="menuItems">
+              <MenuItem className="menuItem">
+                <span
+                  onClick={() => {
+                    console.log("edit board");
+                    openEditBoardModal(selectedBoard);
+                  }}
+                  className=""
+                  style={{ color: "#828fa3" }}
+                >
+                  <PencilIcon style={{ width: "24px", height: "24px" }} />
+                  Edit Board
+                </span>
+              </MenuItem>
+              <MenuItem className="menuItem">
+                <span
+                  onClick={() => {
+                    console.log("delete board");
+                    setIsDeleteModalOpen(true);
+                  }}
+                  className=""
+                  style={{ color: "red" }}
+                >
+                  <TrashIcon style={{ width: "24px", height: "24px" }} />
+                  Delete Board
+                </span>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+        </div>
       </div>
-    </div>
+
+      {isDeleteModalOpen && (
+        <DeleteBoardModal
+          isOpen={isDeleteModalOpen}
+          onClose={() => setIsDeleteModalOpen(false)}
+          onConfirm={deleteBoard}
+          boardName={selectedBoard?.name}
+        />
+      )}
+    </>
   );
 }
