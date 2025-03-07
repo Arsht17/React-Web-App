bafbd6cc8c43478fcd80cdc3594d85fbfd7a5a3e
BuildTools <unconfigured@null.spigotmc.org>
2025-03-07 17:51:09 +0200

    columnsSlice

---
+++ src/store/slices/columnsSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const columnsSlice = createSlice({
+  name: "columns",
+  initialState: {
+    columns: [],
+  },
+  reducers: {
+    setColumns: (state, action) => {
+      state.columns = action.payload;
+    },
+    addColumn: (state, action) => {
+      state.columns.push(action.payload);
+    },
+    editColumn: (state, action) => {
+      const index = state.columns.findIndex((c) => c.id === action.payload.id);
+      if (index !== -1) {
+        state.columns[index] = action.payload;
+      }
+    },
+    deleteColumn: (state, action) => {
+      state.columns = state.columns.filter((c) => c.id !== action.payload);
+    },
+  },
+});
+export default columnsSlice.reducer;
