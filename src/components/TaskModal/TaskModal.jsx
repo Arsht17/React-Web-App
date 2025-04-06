d2cd907d3ac71b19ac76a31975ae925e55b10f09
BuildTools <unconfigured@null.spigotmc.org>
2025-04-06 19:32:26 +0300

    header fix dots

--- src/components/TaskModal/TaskModal.jsx
+++ src/components/TaskModal/TaskModal.jsx
@@ -1,13 +1,38 @@
 import "./TaskModal.scss";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 function TaskModal({ task, onClose }) {
   return (
     <div className="TaskModal" onClick={onClose}>
       <div className="TaskModal-content" onClick={(e) => e.stopPropagation()}>
-        <span className="TaskModal-close" onClick={onClose}>
-          &times;
-        </span>
+        <div className="actions">
+          <Menu>
+            <MenuButton className="dots">
+              <svg
+                className="menu-icon"
+                width="5"
+                height="20"
+                viewBox="0 0 5 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="2.30769" cy="2.30769" r="2.30769" fill="#828FA3" />
+                <circle cx="2.30769" cy="10.0001" r="2.30769" fill="#828FA3" />
+                <circle cx="2.30769" cy="17.6922" r="2.30769" fill="#828FA3" />
+              </svg>
+            </MenuButton>
+            <MenuItems transition className="menuItems">
+              <MenuItem className="menuItem">
+                <span>Edit Task </span>
+              </MenuItem>
+              <MenuItem className="menuItem">
+                <span style={{ color: "#eb0707" }}>Delete Task </span>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+        </div>
         <h3 className="TaskModal-title">{task?.name}</h3>
+        <p className="TaskModal-description">{task?.description}</p>
       </div>
     </div>
   );
