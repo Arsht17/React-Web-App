1038bb4188c7d205e9900ed606d4e1d88ce908ac
BuildTools <unconfigured@null.spigotmc.org>
2026-01-30 14:13:27 +0200

    fix for sass warning

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     preprocessorOptions: {
-      sass: {
-        // Use the modern Sass JS API to avoid the deprecation warning
-        api: "modern",
+      //fix for sass warning
+      scss: {
+        api: "modern-compiler",
       },
     },
   },
